0b0b9bf46aac916f9d8b2b501db72ae6ef446bcd
plin25 <patrick@flowoflogic.com>
2013-10-26 08:19:05 -0400

    Kind of useless but made AA solution 1-line

--- AA/AA.py
+++ AA/AA.py
@@ -1,5 +1,3 @@
 # This is the sequence given here, +1:
 # http://oeis.org/search?q=1%2C4%2C11%2C23%2C45&sort=&language=&go=Search
-x=int(input())
-arr=[0,0,1,4,11,23,45,82,129,208,309]
-print arr[x]+1
+print [0,0,1,4,11,23,45,82,129,208,309][int(input())]+1
